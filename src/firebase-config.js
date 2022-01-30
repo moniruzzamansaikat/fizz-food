5432f44ca32c042ef2c80ba38f17bf1ba1b1d00e
Moniruzzman Saikat <saikat.moniruzzman@gmail.com>
2022-01-31 01:04:12 +0600

    firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCWADqHF_gtNokgvuLk6kWYn5z_P7wE3vs',
-  authDomain: 'fizz-food.firebaseapp.com',
-  projectId: 'fizz-food',
-  storageBucket: 'fizz-food.appspot.com',
-  messagingSenderId: '516737292426',
-  appId: '1:516737292426:web:57e44cd48ad589b0f1b573',
+  // add your firebase config here
 };
 
 export const initFirebase = () => {
